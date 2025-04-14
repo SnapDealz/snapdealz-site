eb29a3eb076f18a42d90a5ba8e5ef42369329097
SnapDealz <sales@snapdealz.world>
2025-04-14 06:29:30 +0100

    changes to SnapDealz

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
-// main.jsx - production content placeholder
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
